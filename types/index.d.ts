5427d05ceb27df1dd4fc39bb4aa43eceba3ec514
nate <npappenhagen@vertafore.com>
2021-05-19 11:35:11 -0600

    added default export to index types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,4 @@ export { default } from './DayPicker';
 export * from './Props';
 export * from './LocaleUtils';
 export * from './DateUtils';
-export * from './Modifiers';
+export { default as DayPickerInput } from './DayPickerInput';
